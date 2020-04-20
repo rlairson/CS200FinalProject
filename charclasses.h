ae82bb42762e125deb8fa101e0279a02e2f4af70
Andrew Calderone <ajcalderone@captechu.edu>
2020-04-19 20:40:05 -0400

    Finished up the charclasses.h file.

--- charclasses.h
+++ charclasses.h
@@ -43,10 +43,98 @@ public:
 	void    printList(const char *skills[], int n);
 };
 
+class Barbarian : public CharClass {
+private:
+	      Barbarian();
+	     ~Barbarian();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Bard : public CharClass {
+private:
+	      Bard();
+	     ~Bard();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Cleric : public CharClass {
+private:
+	      Cleric();
+	     ~Cleric();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Druid : public CharClass {
+private:
+	      Druid();
+	     ~Druid();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Fighter : public CharClass {
+private:
+	      Fighter();
+	     ~Fighter();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Monk : public CharClass {
+private:
+	      Monk();
+	     ~Monk();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Paladin : public CharClass {
+private:
+	      Paladin();
+	     ~Paladin();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Ranger : public CharClass {
+private:
+	      Ranger();
+	     ~Ranger();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Rogue : public CharClass {
+private:
+	      Rogue();
+	     ~Rogue();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Sorcerer : public CharClass {
+private:
+	      Sorcerer();
+	     ~Sorcerer();
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
 class Warlock : public CharClass {
 public:
 	      Warlock();
 	     ~Warlock() {}
+	void  setSkillProfs();
+	void  setToolProfs();
+};
+
+class Wizard : public CharClass {
+public:
+	      Wizard();
+	     ~Wizard() {}
 	void  setSkillProfs();
 	void  setToolProfs();
 };
