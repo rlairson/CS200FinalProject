7b10052458e57dde8b7013b46e2a84766970d893
fruitsnack-hub <62722822+fruitsnack-hub@users.noreply.github.com>
2020-04-21 13:26:15 -0400

    player.h and player.cpp validation
    -Modified player.h to allow main to know if hp, stats, race, and class have been set
    -Removed some defunct functions

--- player.cpp
+++ player.cpp
@@ -1,58 +1,18 @@
 #include"player.h"
 
 Player::Player() {
-	//setRace(1);	//This way there is no class until the user selects it
-	//setClass(1);
 	srand(time(NULL));
 }
-/*
-Player::Player(int pRace, int pSubRace, int pClass) {
-	for (int i = 1; i <= 6; i++) {
-		setRaceStatBonus(i, 0);
-	}
-	setRace(pRace, pSubRace);
-	setClass(pClass);
-	isRaceSet = true;
-	isClassSet = true;
-	srand(time(NULL));
-}*/
 
-/*
-void Player::setPlayerLevel(int level) {
-	playerLevel = level;
-	setProficiencyBonus(playerLevel);
+void Player::printPlayerStats() {
+	cout << "\n\tStrength: " << strength << " (" << (strength  - 10) / 2 << ")"
+		<< "\n\tDexterity: " << dexterity  << " (" << (dexterity  - 10) / 2 << ")"
+		<< "\n\tConstitution: " << constitution  << " (" << (constitution  - 10) / 2 << ")"
+		<< "\n\tIntelligence: " << intelligence  << " (" << (intelligence  - 10) / 2 << ")"
+		<< "\n\tWisdom: " << wisdom  << " (" << (wisdom  - 10) / 2 << ")"
+		<< "\n\tCharisma: " << charisma  << " (" << (charisma  - 10) / 2 << ")"
+		<< endl;
 }
-*/
-
-int Player::getPlayerLevel() {
-	return playerLevel;
-}
-
-/*
-bool Player::getIsRaceSet() {
-	return isRaceSet;
-}
-
-bool Player::getIsClassSet() {
-	return isClassSet;
-}
-
-void Player::setRaceStatBonus(int stat, int amount) {
-	switch (stat) {
-		case 1: raceStr = amount;
-			break;
-		case 2: raceDex = amount;
-			break;
-		case 3: raceCon = amount;
-			break;
-		case 4: raceInt = amount;
-			break;
-		case 5: raceWis = amount;
-			break;
-		case 6: raceCha = amount;
-			break;
-	}
-}*/
 
 int Player::getStat(int num) {
 	switch (num) {
@@ -250,28 +210,11 @@ void Player::swapStat(int first, int second) {
 	}
 }
 
-void Player::printPlayerStats() {
-	cout << "\n\tStrength: " << strength<< " (" << (strength - 10) / 2 << ")"
-		<< "\n\tDexterity: " << dexterity<< " (" << (dexterity - 10) / 2 << ")"
-		<< "\n\tConstitution: " << constitution<< " (" << (constitution - 10) / 2 << ")"
-		<< "\n\tIntelligence: " << intelligence<< " (" << (intelligence - 10) / 2 << ")"
-		<< "\n\tWisdom: " << wisdom<< " (" << (wisdom - 10) / 2 << ")"
-		<< "\n\tCharisma: " << charisma<< " (" << (charisma - 10) / 2 << ")"
-		<< endl;
-}
-
-/*
-void Player::printHitpoints() {
-	cout << "Hitpoints: " << hitpoints << endl;
-}
-*/
-
 int Player::rollDice(int rolls, int sides) {	//(# of dice rolled, # of sides on each dice)
 	int total = 0;
 	for (int i = 0; i < rolls; i++) {
 		total += rand() % sides + 1;
 	}
-	//cout << "Roll: " << total << endl;
 	return total;
 }
 
@@ -284,306 +227,7 @@ void Player::setPlayerStats() {
 	charisma = rollDice(3, 6);
 }
 
-/*
-void Player::setRaceStats(int pRace) {
-	int score1, score2 = 0;
 
-	switch (pRace) {
-		case 1: //Dwarf
-			race = "Dwarf";
-			isRaceSet = true;
-			setRaceStatBonus(3, 2);
-			break;
-		case 2: //Elf
-			race = "Elf";
-			isRaceSet = true;
-			setRaceStatBonus(2, 2);
-			break;
-		case 3: //Halfling
-			race = "Halfling";
-			isRaceSet = true;
-			setRaceStatBonus(2, 2);
-			break;
-		case 4: //Human
-			race = "Human";
-			isRaceSet = true;
-			for (int i = 1; i <= 6; i++) {
-				setRaceStatBonus(i, 1);
-			}
-			break;
-		case 5: //Dragonborn
-			race = "Dragonborn";
-			isRaceSet = true;
-			setRaceStatBonus(1, 2);
-			setRaceStatBonus(6, 1);
-			break;
-		case 6: //Gnome
-			race = "Gnome";
-			isRaceSet = true;
-			setRaceStatBonus(4, 2);
-			break;
-		case 7: //Half-Elf
-			race = "Half-Elf";
-			isRaceSet = true;
-			setRaceStatBonus(6, 2);
-			//2 other scores get +1
-			system("cls");
-			cout << "Half-Elf Ability Score Improvement"
-				<< "\n1. Strength"
-				<< "\n2. Dexterity"
-				<< "\n3. Constitution"
-				<< "\n4. Intelligence"
-				<< "\n5. Wisdom"
-				<< "\n6. Charisma"
-				<< "\n\nSelect two scores to improve by one: ";
-			cin >> score1 >> score2;
-			setRaceStatBonus(score1, 1);
-			setRaceStatBonus(score2, 1);
-			break;
-		case 8: //Half-Orc
-			race = "Half-Orc";
-			isRaceSet = true;
-			setRaceStatBonus(1, 2);
-			setRaceStatBonus(3, 1);
-			break;
-		case 9: //Tiefling
-			race = "Tiefling";
-			isRaceSet = true;
-			setRaceStatBonus(4, 1);
-			setRaceStatBonus(6, 2);
-		default:
-			break;
-	}
-}
-
-void Player::setHitDice(int sides) {
-	hitDice = sides;
-}
-
-int Player::getHitDice() {
-	return hitDice;
-}
-
-int Player::getHitpoints() {
-	return hitpoints;
-}
-
-void Player::setHitpoints(int hp) {
-	hitpoints = hitpoints + hp;
-}
-
-void Player::setClassStats(int pClass) {
-	switch (pClass) {
-		case 1: //Barbarian
-			playerClass = "Barbarian";
-			isClassSet = true;
-			setHitDice(12);
-			//d12
-			break;
-		case 2: //Bard
-			playerClass = "Bard";
-			isClassSet = true;
-			setHitDice(8);
-			break;
-		case 3: //Cleric
-			playerClass = "Cleric";
-			isClassSet = true;
-			setHitDice(8);
-			break;
-		case 4: //Druid
-			playerClass = "Druid";
-			isClassSet = true;
-			setHitDice(8);
-			break;
-		case 5: //Fighter
-			playerClass = "Fighter";
-			isClassSet = true;
-			setHitDice(10);
-			//d10
-			break;
-		case 6: //Monk
-			playerClass = "Monk";
-			isClassSet = true;
-			setHitDice(8);
-			break;
-		case 7: //Paladin
-			playerClass = "Paladin";
-			isClassSet = true;
-			setHitDice(10);
-			//d10
-			break;
-		case 8: //Ranger
-			playerClass = "Ranger";
-			isClassSet = true;
-			setHitDice(10);
-			//d10
-			break;
-		case 9: //Rogue
-			playerClass = "Rogue";
-			isClassSet = true;
-			setHitDice(8);
-			break;
-		case 10: //Sorcerer
-			playerClass = "Sorcerer";
-			isClassSet = true;
-			setHitDice(6);
-			//d6
-			break;
-		case 11: //Warlock
-			playerClass = "Warlock";
-			isClassSet = true;
-			setHitDice(8);
-			//d8
-			break;
-		case 12: //Wizard
-			playerClass = "Wizard";
-			isClassSet = true;
-			setHitDice(6);
-			//d6
-			break;
-		default:
-			break;
-	}
-}
-
-void Player::setRace(int pRace, int pSubRace) {	//Needs second parameter for subrace
-	switch (pRace) {
-		case 1: //Dwarf
-			setRaceStats(1);
-			if (pSubRace == 1) {
-				//Hill Dwarf = 1
-					//Wisdom +1
-				setRaceStatBonus(5, 1);
-				race = "Hill Dwarf";
-			}
-			else if (pSubRace == 2) {
-				//Mountain Dwarf = 2
-					//Strength +2
-				setRaceStatBonus(1, 2);
-				race = "Mountain Dwarf";
-			}
-			else {}	//Error
-			break;
-		case 2: //Elf
-			setRaceStats(2);
-			if (pSubRace == 1) {
-				//High Elf = 1
-					//Intelligence +1
-				setRaceStatBonus(4, 1);
-				race = "High Elf";
-			}
-			else if (pSubRace == 2) {
-				//Wood Elf = 2
-					//Wisdom +1
-				setRaceStatBonus(5, 1);
-				race = "Wood Elf";
-			}
-			else if (pSubRace == 3) {
-				//Dark Elf = 3
-					//Charisma +1
-				setRaceStatBonus(6, 1);
-				race = "Dark Elf";
-			}
-			else{}	//Error
-			break;
-		case 3: //Halfling
-			setRaceStats(3);
-			if (pSubRace == 1) {
-				//Lightfoot = 1
-					//Charisma +1
-				setRaceStatBonus(6, 1);
-				race = "Lightfoot Halfling";
-			}
-			else if (pSubRace == 2) {
-				//Stout
-					//Constitution +1
-				setRaceStatBonus(3, 1);
-				race = "Stout Halfling";
-			}else{}	//Error
-			break;
-		case 4: //Human
-			setRaceStats(4);
-			break;
-		case 5: //Dragonborn
-			setRaceStats(5);
-			//Draconic Ancestry
-			break;
-		case 6: //Gnome
-			setRaceStats(6);
-			if (pSubRace == 1) {
-				//Forest Gnome = 1
-					//Dexterity +1
-				setRaceStatBonus(2, 1);
-				race = "Forest Gnome";
-			}
-			else if (pSubRace == 2) {
-				//Rock Gnome = 1
-					//Constitution +1
-				setRaceStatBonus(3, 1);
-				race = "Rock Gnome";
-			}else{}	//Error
-			break;
-		case 7: //Half-Elf
-			setRaceStats(7);
-			break;
-		case 8: //Half-Orce
-			setRaceStats(8);
-			break;
-		case 9: //Tiefling
-			setRaceStats(9);
-			break;
-		default:
-			break;
-	}
-}
-
-void Player::setClass(int pClass) {
-	switch (pClass) {
-		case 1: //Barbarian
-			setClassStats(1);
-			break;
-		case 2: //Bard
-			setClassStats(2);
-			break;
-		case 3: //Cleric
-			setClassStats(3);
-			break;
-		case 4: //Druid
-			setClassStats(4);
-			break;
-		case 5: //Fighter
-			setClassStats(5);
-			break;
-		case 6: //Monk
-			setClassStats(6);
-			break;
-		case 7: //Paladin
-			setClassStats(7);
-			break;
-		case 8: //Ranger
-			setClassStats(8);
-			break;
-		case 9: //Rogue
-			setClassStats(9);
-			break;
-		case 10: //Sorcerer
-			setClassStats(10);
-			break;
-		case 11: //Warlock
-			setClassStats(11);
-			break;
-		case 12: //Wizard
-			setClassStats(12);
-			break;
-		default:
-			break;
-	}
-}
-
-string Player::getCharacter() {
-	return "Race: " + race + "\nClass: " + playerClass;
-}
-*/
 
 int Player::getStr() {
 	return strength;
@@ -609,7 +253,6 @@ int Player::getCha() {
 	return charisma;
 }
 
-/*
 bool Player::getIsHPSet() {
 	return isHPSet;
 }
@@ -617,7 +260,6 @@ bool Player::getIsHPSet() {
 void Player::setIsHPSet(bool b) {
 	isHPSet = b;
 }
-*/
 
 bool Player::getAreScoresRolled() {
 	return areScoresRolled;
@@ -627,47 +269,16 @@ void Player::setAreScoresRolled(bool b) {
 	areScoresRolled = b;
 }
 
-/*
-void Player::setProficiencyBonus(int level) {
-	switch (level) {
-		case 1:
-		case 2:
-		case 3:
-		case 4:
-			proficiencyBonus = 2;
-			break;
-		case 5:
-		case 6:
-		case 7:
-		case 8:
-			proficiencyBonus = 3;
-			break;
-		case 9:
-		case 10:
-		case 11:
-		case 12:
-			proficiencyBonus = 4;
-			break;
-		case 13:
-		case 14:
-		case 15:
-		case 16:
-			proficiencyBonus = 5;
-			break;
-		case 17:
-		case 18:
-		case 19:
-		case 20:
-			proficiencyBonus = 6;
-			break;
-		default:
-			break;
-	}
+bool Player::getIsRaceSet() {
+	return isRaceSet;
 }
-*/
+void Player::setIsRaceSet(bool b) {
+	isRaceSet = b;
+}
 
-/*
-int Player::getProficiencyBonus() {
-	return proficiencyBonus;
+bool Player::getIsClassSet() {
+	return isClassSet;
 }
-*/
+void Player::setIsClassSet(bool b) {
+	isClassSet = b;
+}
--- player.h
+++ player.h
@@ -9,35 +9,13 @@ using namespace std;
 class Player {
 	public:
 		Player();
-		Player(int pRace, int pSubRace, int pClass);
-
-		//void setRace(int pRace, int pSubRace);
-		//void setClass(int pClass);
-		//void setClassStats(int pClass);
-		//void setRaceStats(int pRace);
 		
 		void setPlayerStats();	//Get random stat scores
+		void printPlayerStats();
 		int rollDice(int rolls, int sides);	//Roll 3 six-sided dice
 		void swapStat(int first, int second);
 
-		//void setRaceStatBonus(int stat, int amount);	//Set racial stat bonus
-
-		void printPlayerStats();
 		int getStat(int num);	//Returns the stat based on number passed
-		
-		//string getCharacter();
-		
-		//bool getIsClassSet();	//Returns isClassSet
-		//bool getIsRaceSet();	//Returns isRaceSet
-		//void setHitDice(int sides);	//Set class hit dice
-		//void rollHP(int rolls, int sides);
-		//int getHitDice();
-		//int getHitpoints();
-		//void setHitpoints(int hp);
-		//void printHitpoints();
-
-		void setPlayerLevel(int level);
-		int getPlayerLevel();
 
 		int getStr();
 		int getDex();
@@ -46,31 +24,24 @@ class Player {
 		int getWis();
 		int getCha();
 
-		//bool getIsHPSet();
-		//void setIsHPSet(bool b);
+		bool getIsHPSet();
+		void setIsHPSet(bool b);
+
+		bool getIsRaceSet();
+		void setIsRaceSet(bool b);
+
+		bool getIsClassSet();
+		void setIsClassSet(bool b);
 
 		bool getAreScoresRolled();
 		void setAreScoresRolled(bool b);
 
-		//void setProficiencyBonus(int level);
-		//int getProficiencyBonus();
-
 
 	private:
-		//bool isClassSet = false;
-		//bool isRaceSet = false;
-		//bool isHPSet = false;
+		bool isClassSet = false;
+		bool isRaceSet = false;
+		bool isHPSet = false;
 		bool areScoresRolled = false;
-
-		int playerLevel = 0;
-		int proficiencyBonus = 0;
-
-		//string race = "";
-		//string playerClass = "";
-
-		//Class
-		//int hitDice = 0;
-		//int hitpoints = 0;
 
 		//Stats
 		int strength = 0;
@@ -79,12 +50,4 @@ class Player {
 		int intelligence = 0;
 		int wisdom = 0;
 		int charisma = 0;
-
-		//Race stat bonus
-		//int raceStr = 0;
-		//int raceDex = 0;
-		//int raceCon = 0;
-		//int raceInt = 0;
-		//int raceWis = 0;
-		//int raceCha = 0;
 };
